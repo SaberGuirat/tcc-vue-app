86b6eb5a97f600d153aa8e757cef2991614aba75
saber guirat <saber.guirat92@gmail.com>
2021-10-08 08:59:58 +0200

    typeScript + vutify setup + refactor vutify components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,137 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify firstName"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>{{ name }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <UsersTable :persons="persons" :headers="headers" />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
+import UsersTable from "./components/UsersTable.vue";
+import Component from "vue-class-component";
+import { IPerson } from "./interfaces/person.interface";
 
-export default {
-  name: 'App',
+@Component({
   components: {
-    HelloWorld
-  }
+    UsersTable,
+  },
+})
+export default class App extends Vue {
+  name : string = "App";
+  headers = [
+    {
+      text: "ID",
+      align: "start",
+      filterable: true,
+      value: "_id",
+    },
+    { text: "First Name", value: "firstName" },
+    { text: "Last Name", value: "lastName" },
+    { text: "Email", value: "email" },
+    { text: "Actions", value: "actions", sortable: false },
+  ];
+  persons : IPerson[]= [
+    {
+      _id: "1",
+      firstName: "Frozen Yogurt",
+      lastName: "Frozen Yogurt@sdsdd.sdsd",
+      email: "Frozen Yogurt@sdsdd.sdsd",    },
+    {
+      _id: "1",
+      firstName: "Ice cream sandwich",
+      lastName: "Frozen Yogurt",
+      email: "Frozen Yogurt@sdsdd.sdsd",
+
+    },
+    {
+      _id: "1",
+      firstName: "Eclair",
+      lastName: "Frozen Yogurt",
+
+      email: "Frozen Yogurt@sdsdd.sdsd",
+    },
+    {
+      _id: "1",
+      firstName: "Cupcake",
+      lastName: "Frozen Yogurt",
+      email: "Frozen Yogurt@sdsdd.sdsd",
+
+    },
+    {
+      _id: "1",
+      firstName: "Gingerbread",
+      lastName: "Frozen Yogurt",
+
+      email: "Frozen Yogurt@sdsdd.sdsd",
+    },
+    {
+      _id: "1",
+      firstName: "Jelly bean",
+      lastName: "Frozen Yogurt",
+      email: "Frozen Yogurt@sdsdd.sdsd",
+
+    },
+    {
+      _id: "1",
+      firstName: "Lollipop",
+      lastName: "Frozen Yogurt",
+      email: "Frozen Yogurt@sdsdd.sdsd",
+
+    },
+    {
+      _id: "1",
+      firstName: "Honeycomb",
+      lastName: "Frozen Yogurt",
+
+      email: "Frozen Yogurt@sdsdd.sdsd",
+    },
+    {
+      _id: "1",
+      firstName: "Donut",
+      lastName: "Frozen Yogurt",
+
+      email: "Frozen Yogurt@sdsdd.sdsd",
+    },
+    {
+      _id: "1",
+      firstName: "KitKat",
+      lastName: "Frozen Yogurt",
+      email: "Frozen Yogurt@sdsdd.sdsd",
+    },
+  ];
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,195 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="persons" :search="search">
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>My CRUD</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  New Item
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="3">
+                        <v-text-field
+                          v-model="editedPerson.firstName"
+                          label="First Name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="3">
+                        <v-text-field
+                          v-model="editedPerson.lastName"
+                          label="Last Name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="12" md="6">
+                        <v-text-field
+                          v-model="editedPerson.email"
+                          label="Email"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="save">
+                    Save
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5"
+                  >Are you sure you want to delete this item?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">
+            Reset
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { IPerson } from "../interfaces/person.interface";
+
+@Component({
+  props: {
+    persons: Array as () => IPerson[],
+    headers: Array,
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+})
+export default class UsersTable extends Vue {
+  persons: IPerson[];
+  editedIndex = -1;
+  editedPerson: IPerson = {
+    firstName: "",
+    lastName: "",
+    email: "",
+  };
+  defaultPerson: IPerson = {
+    firstName: "",
+    lastName: "",
+    email: "",
+  };
+
+  dialog = false;
+  dialogDelete = false;
+
+  data() {
+    return {
+      search: "",
+    };
+  }
+
+  get formTitle() {
+    return this.editedIndex === -1 ? "New Item" : "Edit Item";
+  }
+
+  editItem(person : IPerson) {
+    this.editedIndex = this.persons.indexOf(person);
+    this.editedPerson = Object.assign({}, person);
+    this.dialog = true;
+  }
+
+  deletePerson(person : IPerson) {
+    this.editedIndex = this.persons.indexOf(person);
+    this.editedPerson = Object.assign({}, person);
+    this.dialogDelete = true;
+  }
+
+  deleteItemConfirm() {
+    this.persons.splice(this.editedIndex, 1);
+    this.closeDelete();
+  }
+
+  close() {
+    this.dialog = false;
+    this.$nextTick(() => {
+      this.editedPerson = Object.assign({}, this.defaultPerson);
+      this.editedIndex = -1;
+    });
+  }
+
+  closeDelete() {
+    this.dialogDelete = false;
+    this.$nextTick(() => {
+      this.editedPerson = Object.assign({}, this.defaultPerson);
+      this.editedIndex = -1;
+    });
+  }
+
+  save() {
+    if (this.editedIndex > -1) {
+      Object.assign(this.persons[this.editedIndex], this.editedPerson);
+    } else {
+      this.persons.push(this.editedPerson);
+    }
+    this.close();
+  }
+}
+</script>
