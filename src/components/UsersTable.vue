a014ee3abb63d6c01c29aae9d7e9373f24d4e592
saber guirat <saber.guirat92@gmail.com>
2021-10-24 11:16:16 +0200

    setup tests + docker container with ngnix

--- src/components/UsersTable.vue
+++ src/components/UsersTable.vue
@@ -24,6 +24,7 @@
                   class="mb-2"
                   v-bind="attrs"
                   v-on="on"
+                  id="addUser"
                 >
                   Add User
                 </v-btn>
--- tests/unit/UsersTable.spec.ts
+++ tests/unit/UsersTable.spec.ts
@@ -2,6 +2,38 @@ import { mount, createLocalVue } from "@vue/test-utils";
 import UsersTable from "@/components/UsersTable.vue";
 import Vue from "vue";
 import Vuetify from "vuetify";
+import { IPerson } from "@/interfaces/person.interface";
+
+const persons: IPerson[] = [
+  {
+    _id: "123",
+    firstName: "saber",
+    lastName: "Guirat",
+    email: "saber.guirat92@gmail.com",
+  },
+];
+
+const headers = [
+  {
+    text: "ID",
+    align: "start",
+    filterable: true,
+    value: "_id",
+  },
+  { text: "First Name", value: "firstName" },
+  { text: "Last Name", value: "lastName" },
+  { text: "Email", value: "email" },
+  { text: "Actions", value: "actions", sortable: false },
+];
+const addUser = (person: IPerson) => {
+  console.log("add user");
+};
+const deleteUser = () => {
+  console.log("delete user");
+};
+const updateUser = () => {
+  console.log("update user");
+};
 
 describe("UsersTable.vue", () => {
   const localVue = createLocalVue();
@@ -21,35 +53,6 @@ describe("UsersTable.vue", () => {
   };
 
   it("renders props when passed", async () => {
-    const headers = [
-      {
-        text: "ID",
-        align: "start",
-        filterable: true,
-        value: "_id",
-      },
-      { text: "First Name", value: "firstName" },
-      { text: "Last Name", value: "lastName" },
-      { text: "Email", value: "email" },
-      { text: "Actions", value: "actions", sortable: false },
-    ];
-    const persons = [
-      {
-        _id: "123",
-        firstName: "saber",
-        lastName: "Guirat",
-        email: "saber.guirat92@gmail.com",
-      },
-    ];
-    const addUser = () => {
-      console.log("add user");
-    };
-    const deleteUser = () => {
-      console.log("delete user");
-    };
-    const updateUser = () => {
-      console.log("update user");
-    };
     const wrapper = mountFunction({
       propsData: { headers, persons, addUser, deleteUser, updateUser },
     });
